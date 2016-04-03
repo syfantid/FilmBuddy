7abe92c30ed0d48b46143b5fa907ee104b0d7cd3
Sofia Yfantidou <syfantid@csd.auth.gr>
2016-04-03 17:11:47 +0300

    Movies' collection ready to be inserted to DB. Minor bugs due to wikipedia articles' inconsistency.

--- src/movies/MovieCollector.java
+++ src/movies/MovieCollector.java
@@ -2,6 +2,7 @@ package movies;
 
 import jwiki.core.NS;
 import jwiki.core.Wiki;
+import jwiki.dwrap.ImageInfo;
 
 import java.util.ArrayList;
 import java.util.Iterator;
@@ -9,6 +10,7 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 /**
+ * Class to collect all movies and their information from Wikipedia
  * Created by Sofia on 3/31/2016.
  */
 public class MovieCollector {
@@ -19,31 +21,41 @@ public class MovieCollector {
      * @return The extended plot of the film
      */
     private static String getExtendedPlot(String page) {
-        String plot;
-        int index1 = page.indexOf("==Plot=="); // The index of the Plot title in the page
+        String plot = "";
+        String keyword;
+        // Plot is always between the Plot and the Cast section
+        int index1;
+        if(page.contains("==Plot==")) {
+            index1 = page.indexOf("==Plot=="); // The index of the Plot title in the page
+            keyword = "==Plot==";
+        } else if(page.contains("==Synopsis==")) {
+            index1 = page.indexOf("==Synopsis=="); // The index of the Plot title in the page
+            keyword = "==Synopsis==";
+        } else {
+            return plot;
+        }
         int index2; // The index of the Cast title in the page
 
         if(page.contains("==Cast==")) { // Cast title is "Cast"
             index2 = page.indexOf("==Cast==");
             if(index1 < index2) {
-                plot = page.substring(index1 + "==Plot==".length(),
+                plot = page.substring(index1 + keyword.length(),
                         index2);
-            } else { // In a few articles the cast section precedes the plot section, thus not marking its ending
-                plot = "";
             }
         } else if(page.contains("==Principal cast==")) { // Cast title is "Principal Cast"
             index2 = page.indexOf("==Principal cast==");
-            plot = page.substring(index1 + "==Plot==".length(),
-                    index2);
+            if(index1 < index2) {
+                plot = page.substring(index1 + keyword.length(),
+                        index2);
+            }
         } else if(page.contains("==Main cast==")){ // Cast title is "Main Cast"
             index2 = page.indexOf("==Main cast==");
-            plot = page.substring(index1 + "==Plot==".length(),
-                    index2);
-        } else { // There's no cast section to mark the ending of plot section
-            plot = "";
+            if(index1 < index2) {
+                plot = page.substring(index1 + keyword.length(),
+                        index2);
+            }
         }
-
-        return plot;
+        return plot.replaceAll("[\\[\\]]","");
     }
 
     /**
@@ -52,9 +64,8 @@ public class MovieCollector {
      * @return The production year of the films in the given category
      */
     private static String getYear(String category) {
-        String year = category.substring(category.indexOf("Category:") + "Category:".length(),
+        return category.substring(category.indexOf("Category:") + "Category:".length(),
                 category.indexOf(" films"));
-        return year;
     }
 
     /**
@@ -63,9 +74,9 @@ public class MovieCollector {
      * @return The name of the director
      */
     private static String getDirector(String page) {
-        Matcher m = Pattern.compile("director = ([\\w\\s]*)").matcher(page);
+        Matcher m = Pattern.compile("director\\s*=\\s*([(\\w\\s\\u00E0-\\u00FC).\\[\\]]*)").matcher(page);
         if(m.find()) {
-            return m.group(1);
+            return m.group(1).replaceAll("[\\[\\]]","");
         }
         return "";
     }
@@ -77,10 +88,14 @@ public class MovieCollector {
      */
     private static String getStars(String page) {
         String stars = "";
-        Matcher m = Pattern.compile("starring = (\\[\\[[\\w\\s]*\\]\\](<br>)?)*").matcher(page);
+        Matcher m = Pattern
+                .compile("(?i)starring\\s*=\\s*((\\{\\{Plainlist\\| \\* )?(\\[\\[[\\w\\s\\u00E0-\\u00FC|]*\\]\\]" +
+                        "(<br>| \\* |(\\s*<br\\s*/>\\s*))?)*)")
+                .matcher(page);
         if(m.find()) {
-            stars = m.group();
-            stars = stars.replaceAll("<br>",", ").replaceAll("[\\[\\]]","").replace("starring = ","");
+            stars = m.group(1);
+            stars = stars.replaceAll("(?i)<br>| \\* |(\\s*<br\\s*/>\\s*)",", ").replaceAll("[\\[\\]]","")
+                    .replace("{{Plainlist|, ",""); //Different format of Info-Box
         }
         return stars;
     }
@@ -92,52 +107,159 @@ public class MovieCollector {
      */
     private static String getSynopsis(String page) {
         String synopsis = "";
-        Matcher m = Pattern.compile("}} [\\w\\d\\s.,\\/#!$%&;:{}=\\-_`'\"~()]* ==").matcher(page);
+        // From the end of Info-box to the start of a new section/heading
+        page = page.replaceAll("<ref>([\\w\\s\\u00E0-\\u00FC\\[\\]{}()|:?\\-=%&;'\"/.,]*)</ref>",""); // Remove all links
+        Matcher m = Pattern.compile("}} ([\\w\\d\\s\\u00E0-\\u00FC.,\\[\\]\\/\\|#!%<>?’&;:{}\\-_`'\"~()]*?) ={2,}")
+                .matcher(page);
         if(m.find()) {
-            synopsis = m.group();
-            synopsis = synopsis.replaceAll("'{2,}","\"").replace("}}","").replace("==","");
+            synopsis = m.group(1);
+            synopsis = synopsis.replaceAll("'{2,}","\"").replaceAll("\\[\\[[\\w\\s()]*\\|","")
+                    .replaceAll("[\\[\\]]","");
         }
         return synopsis;
 
+    }
+
+    /**
+     * Gets the IMDb URI of a film
+     * @param page The film's Wikipedia page text
+     * @return The IMDb URI
+     */
+    private static String getIMDbLink(String page) {
+        String link = "";
+        String code;
+        Matcher m = Pattern.compile("(?i)\\{\\{IMDb title\\|(id\\s*=)?\\s*(\\d*\\w*)").matcher(page);
+        if(m.find()) {
+            code = m.group(2).replaceAll("\\s+","");
+            link = "http://www.imdb.com/title/tt" + code;
+        }
+        return link;
+    }
+
+    /**
+     * Gets the Wikipedia poster's URL of a film
+     * @param page The film's Wikipedia page text
+     * @param wiki The Wikipedia connection object
+     * @return The URL of the Wikipedia poster
+     */
+    private static String getIconURL(String page, Wiki wiki) {
+        // TODO: 4/3/2016 ImageInfo Class bug probably
+        String imageName;
+        String iconURL = "";
+        Matcher m = Pattern.compile("(?i)image\\s*=\\s*([\\w\\d\\s.]*)").matcher(page); // Get the image file name
+        if(m.find()) {
+            imageName = m.group(1).replaceAll("\\s+","");
+            ImageInfo image = wiki.getImageInfo(imageName);
+            if(image != null) {
+                iconURL = image.url;
+            }
+        }
+        return iconURL;
+    }
+
+    /**
+     * Gets the Wikipedia categories of a film
+     * @param page The film's Wikipedia page text
+     * @return A list of all the related categories
+     */
+    private static ArrayList<String> getCategories(String page) {
+        ArrayList<String> categories = new ArrayList<>();
+        Matcher m = Pattern.compile("\\[\\[Category:\\s*([\\w\\d\\s\\u00E0-\\u00FC-]*)\\]\\]*").matcher(page);
+        while(m.find()) {
+            categories.add(m.group(1));
+
+        }
+        return categories;
     }
 
     public static void main(String[] args) throws Throwable
     {
         Wiki wiki = new Wiki("syfantid", "sofia24041994", "en.wikipedia.org"); // Login to Wikipedia
 
-        // Numbered are the fields that will be stored in the DB
-        // TODO: 3/31/2016 Get all the categories
-        String category = "Category:2000 films";
+        // THIS IS A TESTING PART
+
+        /*String category = "Category:2000 films";
         String year = getYear(category); // 1. The year for all the movies in the specific category
         ArrayList<String> films = wiki.getCategoryMembers(category,NS.MAIN); // Get all the articles in this category
 
-
-        // THIS IS A TESTING PART
-        String titleTest = films.get(200); // Film title
+        String titleTest = films.get(10); // Film title
         String pageTestFormatted = wiki.getPageText(titleTest); // The text of the article
         String pageTest = pageTestFormatted.replaceAll("\\s+", " ");
         String extendedPlotTest = getExtendedPlot(pageTest); // The extended plot of the film
         if(!extendedPlotTest.isEmpty()) {
             System.out.println("Page for film: " + titleTest);
-            //System.out.println(pageTest);
+            System.out.println(pageTestFormatted);
+            System.out.println(pageTest);
+            System.out.println("STARS");
+            System.out.println(getStars(pageTest));
+            System.out.println("DIRECTOR");
+            System.out.println(getDirector(pageTest));
+            System.out.println("SYNOPSIS");
             System.out.println(getSynopsis(pageTest));
-        }
-
-        // THIS IS THE NORMAL CODE
-        /*Iterator<String> it = films.iterator();
-        while (it.hasNext()) {
-            String f = it.next();
-            if(f.startsWith("List of")) { // Categories include lists of special films, which will be excluded
-                it.remove();
-            } else {
-                String title = f; // 2. Film title
-                String pageFormatted = wiki.getPageText(title); // The text of the article
-                String page = pageFormatted.replaceAll("\\s+", " "); // Remove any extra whitespaces
-                String extendedPlot = getExtendedPlot(page); // 3. The extended plot of the film
-                if(!extendedPlot.isEmpty()) { // Exclude films with empty Wikipedia pages
-                    // Get all the extra information needed to present the film
-                }
+            System.out.println("LINK");
+            System.out.println(getIMDbLink(pageTest));
+            System.out.println("IMAGE");
+            //String URL = getIconURL(pageTest,wiki);
+            System.out.println("CATEGORIES");
+            ArrayList<String> categories = getCategories(pageTest);
+            for(String s : categories) {
+                System.out.print(s + ", ");
             }
+            System.out.println("EXTENDED PLOT");
+            System.out.println(extendedPlotTest);
+
         }*/
+
+        // TODO: 3/31/2016 Get all the categories
+
+        int yearNumber = 1900;
+        while(yearNumber <= 2016) {
+            String category = "Category:" + yearNumber + " films";
+            ArrayList<String> films = wiki.getCategoryMembers(category,NS.MAIN); // Get all the articles in this category
+
+            Iterator<String> it = films.iterator();
+            while (it.hasNext()) { // Iterate through the year's movies
+                String title = it.next();
+                if(title.startsWith("List of")) { // Categories include lists of special films, which will be excluded
+                    it.remove();
+                } else {
+                    String pageFormatted = wiki.getPageText(title); // The text of the article formatted
+                    String page = pageFormatted.replaceAll("\\s+", " "); // Remove any extra whitespaces
+                    String extendedPlot = getExtendedPlot(page); // The extended plot of the film
+                    if(!extendedPlot.isEmpty()) { // Exclude films with empty Wikipedia pages
+                        // Get all the extra information needed to present the film (optional information)
+                        ArrayList<String> categories = getCategories(page);
+                        String synopsis = getSynopsis(page);
+                        String iconURL = getIconURL(page, wiki);
+                        String cast = getStars(page);
+                        String director = getDirector(page);
+                        String imdbURL = getIMDbLink(page);
+                        //String sematicsPlot = MovieSemantics.getSemanticsPlot(extendedPlot);
+                        System.out.println("Page for film: " + title);
+                        System.out.println(page);
+                        System.out.println("STARS");
+                        System.out.println(cast);
+                        System.out.println("DIRECTOR");
+                        System.out.println(director);
+                        System.out.println("SYNOPSIS");
+                        System.out.println(synopsis);
+                        System.out.println("LINK");
+                        System.out.println(imdbURL);
+                        //System.out.println("IMAGE");
+                        //String URL = getIconURL(pageTest,wiki);
+                        System.out.println("CATEGORIES");
+                        for(String s : categories) {
+                            System.out.print(s + ", ");
+                        }
+                        System.out.println();
+                        System.out.println("EXTENDED PLOT");
+                        System.out.println(extendedPlot);
+                        System.out.println();
+                    }
+                }
+                System.out.println("***********************************************************************************");
+            }
+            yearNumber++; // Parse next year's movies
+        }
     }
 }
---
+++ src/movies/MovieSemantics.java
@@ -0,0 +1,12 @@
+package movies;
+
+/**
+ * Class to mine the semantics of the extended movie plot
+ * Created by Sofia on 4/3/2016.
+ */
+public class MovieSemantics {
+    // TODO: 4/3/2016 Complete the semantics generation 
+    public static String getSemanticsPlot(String extendedPlot) {
+        return "";
+    }
+}
